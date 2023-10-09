270d321e52b2b136911b5df3f4cbf915437cac2a
sufiibadah02 <sufiibadah86@gmail.com>
2023-10-09 16:28:16 +0700

    membuat template sederhana dan dinamis dan setting base_url()

---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+
+<section> 
+<h1><?php echo $judul ?></h1>
+<h4>Nama</h4> 
+    <ul type="disc"> 
+        <li>Nama Depan : Sufi</li> 
+        <li>Nama Belakang : Ibadah</li> 
+    </ul> 
+    <br> 
+    <h4>Alamat</h4> 
+    <ul type="none"> 
+        <li> JL.KH.Syahdan No.11 Rt.002 Rw.011 Kec</li> 
+    </ul> 
+     
+    <h4>Tempat Lahir</h4> 
+    <ul type="none"> 
+        <li>Jakarta</li> 
+    </ul> 
+ 
+    <h4>Olah Raga Favorit</h4> 
+    <ul type="square"> 
+        <li>Bulutangkis</li> 
+        <li>Catur</li> 
+    </ul> 
+</section>
