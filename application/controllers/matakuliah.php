522e006e1d2c1ee5244f21311fe91fdca18fedb5
sufiibadah02 <sufiibadah86@gmail.com>
2023-10-16 15:34:33 +0700

    membuat repository baru di Github

--- application/controllers/matakuliah.php
+++ application/controllers/matakuliah.php
@@ -1,21 +1,39 @@
-<?php 
-class Matakuliah extends CI_Controller 
-{ 
- 
-    public function index() 
-    { 
- 
-        $this->load->view('view-form-matakuliah'); 
-    } 
- 
-    public function cetak() 
-    { 
-        $data = [ 
-            'kode' => $this->input->post('kode'), 
-            'nama' => $this->input->post('nama'), 
-            'sks' => $this->input->post('sks') 
-        ]; 
- 
-        $this->load->view('view-data-matakuliah', $data); 
-    } 
+<?php
+class Matakuliah extends CI_Controller
+{
+    public function index ()
+    {
+        $this->load->view('view-form-matakuliah');
+    }
+
+    public function cetak()
+    {
+        $this->form_validation->set_rules('kode', 'Kode Matakuliah', 'required|min_length[3]', 
+        [
+            'required' => 'Kode Matakuliah Kudu diisi',
+            'min_length' => 'Kode terlalu pendek'
+        ]);
+
+        $this->form_validation->set_rules('nama', 'Nama Matakuliah', 'required|min_length[3]', 
+        [
+            'required'=>'Nama Matakuliah Kudu diisi',
+            'min_length'=>'Nama terlalu pendek'
+        ]);
+
+        $this->form_validation->set_rules('sks', 'SKS', 'required', 
+        [
+            'required'=>'Pilih sesuai SKS'
+        ]);
+        if ($this->form_validation->run() !=false) 
+        {
+            $data = [
+                'kode' => $this->input->post('kode'),
+                'nama' => $this->input->post('nama'),
+                'sks' => $this->input->post('sks')
+            ];
+            $this->load->view('view-data-matakuliah', $data);
+        } else { 
+            $this->load->view('view-form-matakuliah');
+        }
+    }
 } 
--- application/views/view-form-matakuliah.php
+++ application/views/view-form-matakuliah.php
@@ -1,55 +1,64 @@
-<html> 
-<head> 
-        <title>Form Input Matakuliah</title> 
-</head> 
-<body> 
- <center> 
-         <form action="<?= base_url('matakuliah/cetak'); ?>" method="post"> 
-<table> 
-        <tr> 
-            <th colspan="3"> 
-                Form Input Data Mata Kuliah 
-            </th> 
-        </tr>
-        <tr> 
-                    <td colspan="3"> 
-                        <hr> 
-                    </td> 
-                </tr> 
-                <tr> 
-                    <th>Kode MTK</th> 
-                    <th>:</th> 
-                    <td> 
-                        <input type="text" name="kode" id="kode"> 
-                    </td> 
-                </tr> 
-                <tr> 
-                    <th>Nama MTK</th> 
-                    <td>:</td> 
-                    <td> 
-                        <input type="text" name="nama" id="nama"> 
-                    </td> 
-                </tr> 
-                <tr> 
-                    <th>SKS</th> 
-                    <td>:</td> 
-                    <td> 
-                        <select name="sks" id="sk>
-                        <option value="">Pilih SKS</option> 
-                            <option value="2">2</option> 
-                            <option value="3">3</option> 
-                            <option value="4">4</option> 
-                        </select> 
-                    </td> 
-                </tr> 
-                <tr> 
-                    <td colspan="3" align="center"> 
-                        <input type="submit" value="Submit"> 
-                    </td> 
-                </tr> 
-            </table> 
-        </form> 
-</center> 
-</body> 
- 
+<<<html>
+
+<head>
+    <title>Form Input Matakuliah</title>
+</head>
+
+<body>
+    <center>
+        <form action="<?= base_url('matakuliah/cetak'); ?>" method="post">
+        
+            <table>
+                <tr>
+                    <th colspan="3">
+                        Form Input Data Matakuliah
+                    </th>
+                </tr>
+                <tr>
+                    <td colspan="3">
+                        <hr>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Kode MTK</th>
+                    <td>:</td>
+                    <td>
+                        <input type="text" name="kode" id="kode">
+                        <br>
+                        <?= form_error('kode', '<small style="color:red">','</small>');?>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Nama MTK</th>
+                    <td>:</td>
+                    <td>
+                        <input type="text" name="nama" id="nama">
+                        <br>
+                        <?= form_error('nama','<small style="color:red">','</small>');?>
+                    </td>
+                </tr>
+                <tr>
+                    <th>SKS</th>
+                    <td>:</td>
+                    <td>
+                        <select name="sks" id="sks"> 
+                            <option value="">Pilih SKS</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                        </select>
+                        <br>
+                        <?= form_error('sks','<small style="color:red">','</small>');?>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan='3' align='center'>
+                        <input type='submit' value='Submit'>
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </center>
+</body>
+
 </html>
